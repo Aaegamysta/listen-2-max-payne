ba446f31481400f87496034248357e3d6d050255
aaegamysta <aaegamysta@protonmail.com>
2024-09-10 20:19:40 +0200

    added implementation of linked list backed queue

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -40,11 +40,11 @@ func (q *arrayBackedQueue) Dequeue() (data.Excerpt, bool) {
 }
 
 func (q *arrayBackedQueue) Enqueue(e data.Excerpt) (int, bool) {
-	if q.rear == len(q.arr) - 1 {
+	if q.rear == len(q.arr)-1 {
 		return q.rear, false
 	}
 	q.rear++
-	q.arr[q.rear] = &e	
+	q.arr[q.rear] = &e
 	return q.rear, true
 }
 
@@ -56,8 +56,79 @@ func (q *arrayBackedQueue) Empty() bool {
 }
 
 func (q *arrayBackedQueue) Full() bool {
-	if q.rear == len(q.arr) - 1 {
+	if q.rear == len(q.arr)-1 {
 		return true
 	}
 	return false
+}
+
+type node struct {
+	next  *node
+	value *data.Excerpt
+}
+
+type linkedListBackedQueue struct {
+	head  *node
+	front *node
+	rear  *node
+	length int
+	size  int
+}
+
+func NewLinkedListBackedQueue(size int) Queue {
+	var head, front, rear *node
+	return &linkedListBackedQueue{
+		head:  head,
+		front: front,
+		rear:  rear,
+		length: 0,
+		size:  size,
+	}
+}
+
+func (q *linkedListBackedQueue) Empty() bool {
+	if q.rear == nil {
+		return true
+	}
+	return false
+}
+
+func (q *linkedListBackedQueue) Full() bool {
+	if q.length == q.size {
+		return true
+	}
+	return false
+}
+
+func (q *linkedListBackedQueue) Dequeue() (data.Excerpt, bool) {
+	if q.front == nil {
+		return data.Excerpt{}, false
+	}
+	e := q.front
+	q.front = q.front.next
+	q.length--
+	return *e.value, false
+}
+
+func (q *linkedListBackedQueue) Enqueue(e data.Excerpt) (int, bool) {
+	if q.Full() {
+		return q.size, false
+	}
+	if q.rear == nil {
+		q.rear = &node{
+			next: nil,
+			value: &e,
+		}
+		q.front = q.rear 
+		q.length++
+		return q.size, true
+	}
+	n := q.rear
+	n.next = &node{
+		next:  nil,
+		value: &e,
+	}	
+	q.rear = n.next
+	q.length++
+	return q.size, true
 }
