eec69e7e7ba361e5e8b34cc91089647474227bc8
aaegamysta <aaegamysta@protonmail.com>
2024-09-11 17:24:48 +0200

    added the implementation of CreateTableIfNotExists in repository

--- internal/data/repository.go
+++ internal/data/repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/aaegamysta/listen-2-max-payne/internal/twitter"
+	"github.com/jackc/pgx/v5"
 	"go.uber.org/zap"
 )
 
@@ -31,7 +32,43 @@ func New(cfg Config, logger *zap.SugaredLogger) Interface {
 }
 
 func (i *Impl) CreateTablesIfNotExists(ctx context.Context) error {
-	panic("unimplemented")
+	conn, err := pgx.Connect(ctx, i.connectionString)
+	if err != nil {
+		i.logger.Panicf("something wrong happened while connecting to the database: %v", err)
+	}
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		i.logger.Panicf("something wrong happened while starting transaction for creating tables: %v", err)
+	}
+	_, err = tx.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS excerpts (
+			series text, part text, chapter text, excerpt text,
+			PRIMARY KEY (excerpt)
+	);`)
+	if err != nil {
+		i.logger.Panicf("something wrong happened while creating excerpts table: %v", err)
+	}
+	_, err = tx.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS successfull_tweet_response (
+			posted_on timestamp PRIMARY KEY, tweeted_excerpt TEXT, tweet_id TEXT, edit_history_tweet_ids JSONB,
+			FOREIGN KEY (tweeted_excerpt) REFERENCES excerpts(excerpt)
+	);`)
+	if err != nil {
+		i.logger.Panicf("something wrong happened while creating successfull_tweet_response table: %v", err)
+	}
+	_, err = tx.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS error_tweet_response (
+			post_failed_on timestamp PRIMARY KEY, title TEXT, type TEXT, detail TEXT, status INT, failed_excerpt TEXT,
+			FOREIGN KEY (failed_excerpt) REFERENCES excerpts(excerpt)
+	);`)
+	if err != nil {
+		i.logger.Panicf("something wrong happened while creating error_tweet_response table: %v", err)
+	}
+	err = tx.Commit(ctx)
+	if err != nil {
+		i.logger.Panicf("something wrong happened while commiting table creation statements: %v", err)
+	}
+	return nil
 }
 
 func (i *Impl) BatchInsertExcerpts(excerpts []Excerpt) ([]Excerpt, error) {
